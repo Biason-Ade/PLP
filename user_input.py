c70bc38386ec426ffd60f1f653be4432cf43ca56
Biason-Ade <141412552+Biason-Ade@users.noreply.github.com>
2024-08-23 09:06:38 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -3,4 +3,4 @@ age = input('Please input your age: ')
 location = input('Please input your current location: ')
 
 
-print(f"Hello {name}, you are {age} years old and you line in {location}."
+print(f"Hello {name}, you are {age} years old and you line in {location}.")
